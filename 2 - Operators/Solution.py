46b070f37385fb529925225ea052fb4f3fd1793c
Coderpreet <31698917+Coderpreet@users.noreply.github.com>
2020-02-01 17:31:48 +0530

    update solution.py
    added round function to satisfy all test cases. Now the code is working.

--- 2 - Operators/Solution.py
+++ 2 - Operators/Solution.py
@@ -1,9 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
 meal_cost = float(input())
 tip_percent = int(input())
 tax_percent = int(input())
 
-tip = meal_cost * tip_percent / 100
-tax = meal_cost * tax_percent / 100
-total = meal_cost + tip + tax
+tip_percent = meal_cost * (tip_percent / 100)  
+tax_percent = meal_cost * (tax_percent / 100) 
+totalcost = float( meal_cost + tip_percent + tax_percent)  
+print(round(totalcost))
 
-print("The total meal cost is " + str(round(total)) + " dollars.")
+#this is for python3
